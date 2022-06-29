1b6c3ad2f332330005a5a4ff34194c93d68f1beb
Nadege Awah <awahnadege02@gmail.com>
2022-06-29 14:47:31 -0400

    Create add.sh

---
+++ add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Author: Nadege
+#Add 5 numbers
+
+echo "My first parameter is $1"
+echo "My second parameter is $2"
+echo "My third parameter is $3"
+echo "My fourth parameter is $4"
+echo "My fifth parameter is $5"
+
+((sum=$1+$2+$3+$4+$5))
+
+echo "Sum of 5 numbers is $sum"
